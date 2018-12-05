95cd20c9d20ae51ff20c394972745f1c28caac58
naoya.inoue <a.good.for.nothing.fellow@gmail.com>
2018-12-05 15:05:04 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,7 @@
         <div class="text-center">
             <h1>Task Management</h1>
             <h2>シンプルなタスク管理</h2>
+            {!! link_to_route('signup.get', '新規登録しますか？', null, ['class' => 'btn btn-lg btn-primary']) !!}
         </div>
     </div>
 @endsection
